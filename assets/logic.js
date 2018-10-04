75452a064c0ed6ee8409be9c683931b08d0663a1
Jesse Schimmel <jhowardschimmel@gmail.com>
2018-10-03 22:21:19 -0400

    generates 10 disaster buttons

--- assets/logic.js
+++ assets/logic.js
@@ -1,52 +1,62 @@
 
-//var queryURL = "https://api.reliefweb.int/v1/disasters?appname=apidoc&limit=10&preset=latest&sort[]=date:desc&sort[]=title:asc&profile=list&fields[include][]=date.original"
+//on ready
+$(document).ready(function () {
 
-//https://api.reliefweb.int/v1/reports?profile=full&limit=1
+    //query
+    var queryURL = "https://api.reliefweb.int/v1/disasters?profile=full&limit=10&sort[]=date:desc"
 
-//var queryURL = "https://api.reliefweb.int/v1/disasters?appname=apidoc&?profile=full&limit=10&preset=latest&sort[]=date:desc&sort[]=title:asc"
+    //ajax call
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
 
-//var queryURL ="https://api.reliefweb.int/v1/reports?appname=apidoc&filter[field]=disaster.name"
-
-//https://api.reliefweb.int/v1/reports?appname=apidoc&limit=3&preset=latest&fields[include][]=url
+        .then(function (response) {
 
 
-var queryURL ="https://api.reliefweb.int/v1/disasters?profile=full&limit=10&sort[]=date:desc"
 
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-})
-
-    .then(function (response) {
-        
-
-        
-        console.log(response.data);
-        
-        for (var i = 0; i < response.data.length; i++) {
-
-            console.log(i);
+            console.log(response.data);
 
             
 
-            //console.log(response.data[i].fields.country[0].name);
-            console.log(response.data[i].fields.name);
+            //iterate through the response data
+            for (var i = 0; i < response.data.length; i++) {
+                
+                var results = response.data;
 
-            console.log(response.data[i].fields.primary_country.name);
-
-            //console.log(response.data[i].fields.title);
-
-            //console.log(response.data[i].fields.href);
-
-            document.write(response.data[i].fields.description-html);
-            
+                createButtons();
 
 
-        }
+                console.log(i);
+                console.log(response.data[i].fields.name);
+                console.log(response.data[i].fields.primary_country.name);
 
 
 
-    });
+                //create 10 buttons
+                function createButtons() {
+                    var button = $('<button>').attr({
+                        class: "ui button"
+                    });
+                    $(button).text(`${results[i].fields.primary_type.name} : ${results[i].fields.primary_country.name}`);
+                    $("#disasterdiv").append(button);
 
-    
+
+
+
+
+
+                }
+
+
+
+
+            }
+
+
+
+        });
+
+
+
+});
